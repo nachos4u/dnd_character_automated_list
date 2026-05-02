3b4ae4347f4f4e0a78bdbed377fa4b800c3b3d8f
NachoS <20chegor06@mail.ru>
2026-05-03 02:40:00 +0300

    List of characters updeted (half working)

--- DnD_character_list/Form3.cs
+++ DnD_character_list/Form3.cs
@@ -7,16 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Xunit.Internal;
 
 namespace DnD_character_list
 {
     public partial class Form3 : Form
     {
-
+        private TableLayoutPanel mainTable;
+        private Label label;
+        private List<GroupOfCharecters> groups = new List<GroupOfCharecters>();
+        private Button btnAddGroup;
+        private BindingList<GroupOfCharecters> items;
+        private PictureBox pictureBox1;
         public Form3(Form1 form1)
         {
             InitializeComponent();
-            this.button1.MouseClick += button1_Click;
+            items = new BindingList<GroupOfCharecters>();
+
+            // Подписка на изменения свойств каждого элемента
+            items.ForEach(item => item.PropertyChanged += Item_PropertyChanged);
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -26,14 +35,6 @@ namespace DnD_character_list
             this.Hide();
 
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Form4 form4 = new Form4(1);
-            form4.Show();
-            this.Hide();
-        }
-
         private void Form3_FormClosed(object sender, FormClosedEventArgs e)
         {
             Application.Exit();
@@ -56,169 +57,105 @@ namespace DnD_character_list
 
         }
 
-        private void Form3_Load(object sender, EventArgs e)
+        private void BtnAddGroup_Click(object sender, EventArgs e)
         {
-            using (var bd = new DDInformationContext()) {
-                var character = bd.Characters.ToList();
-                var listlLable = new List<string>();
-                for (int i = 1; i < 21; i++)
-                {
-                    var charka = bd.Characters.Where(c => c.IdCharacter == i).Select(c => new { c.Name }).FirstOrDefault();
-                    listlLable.Add(charka.Name);
-                }
-                label1.Text = listlLable[0];
-                label2.Text = listlLable[1];
-                label3.Text = listlLable[2];
-                label4.Text = listlLable[3];
-                label5.Text = listlLable[4];
-                label6.Text = listlLable[5];
-                label7.Text = listlLable[6];
-                label8.Text = listlLable[7];
-                label9.Text = listlLable[8];
-                label10.Text = listlLable[9];
-                label11.Text = listlLable[10];
-                label12.Text = listlLable[11];
-                label13.Text = listlLable[12];
-                label14.Text = listlLable[13];
-                label15.Text = listlLable[14];
-                label16.Text = listlLable[15];
-                label17.Text = listlLable[16];
-                label18.Text = listlLable[17];
-                label19.Text = listlLable[18];
-                label20.Text = listlLable[19];
+            int groupIndex = groups.Count + 1;
+            GroupOfCharecters group = new GroupOfCharecters();
+
+            // Устанавливаем индекс и текст кнопки
+            group.Index = groupIndex;
+
+            // Привязываем событие удаления
+            group.OnDelete += (g) =>
+            {
+                mainTable.Controls.Remove(g.Button);
+                groups.Remove(g);
+                g.Dispose();
+                ReindexGroups();
+            };
+
+            groups.Add(group);
+            mainTable.Controls.Add(group.Button);
+            mainTable.RowCount = groups.Count;
+            mainTable.Height = groups.Count * 90;
+
+            // Автопрокрутка вниз
+            mainTable.ScrollControlIntoView(group.Button);
+        }
+
+        private void ReindexGroups()
+        {
+            for (int i = 0; i < groups.Count; i++)
+            {
+                groups[i].Index = i + 1;
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            Form4 form4 = new Form4(2);
-            form4.Show();
-            this.Hide();
-        }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void Form3_Load(object sender, EventArgs e)
         {
-            Form4 form4 = new Form4(3);
-            form4.Show();
-            this.Hide();
-        }
+            this.Size = new Size(700, 550);
 
-        private void button4_Click(object sender, EventArgs e)
-        {
-            Form4 form4 = new Form4(4);
-            form4.Show();
-            this.Hide();
-        }
+            // Верхняя панель
+            Panel topPanel = new Panel();
+            topPanel.Height = 140;
+            topPanel.Dock = DockStyle.Top;
 
-        private void button5_Click(object sender, EventArgs e)
-        {
-            Form4 form4 = new Form4(5);
-            form4.Show();
-            this.Hide();
-        }
+            // PictureBox
+            pictureBox1 = new PictureBox();
+            pictureBox1.BackColor = Color.White;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Center;
+            pictureBox1.Image = Properties.Resources.Untitled;
+            pictureBox1.Location = new Point(10, 10);
+            pictureBox1.Size = new Size(81, 81);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Click += pictureBox1_Click;
 
-        private void button6_Click(object sender, EventArgs e)
-        {
-            Form4 form4 = new Form4(6);
-            form4.Show();
-            this.Hide();
-        }
+            // Label
+            label = new Label();
+            label.AutoSize = true;
+            label.Location = new Point(10, 105);
+            label.Text = "Ваши персонажи";
 
-        private void button7_Click(object sender, EventArgs e)
-        {
-            Form4 form4 = new Form4(7);
-            form4.Show();
-            this.Hide();
-        }
+            // Кнопка добавления
+            btnAddGroup = new Button();
+            btnAddGroup.Text = "➕ Добавить группу";
+            btnAddGroup.Size = new Size(150, 35);
+            btnAddGroup.Location = new Point(100, 10);
+            btnAddGroup.Click += BtnAddGroup_Click;
 
-        private void button8_Click(object sender, EventArgs e)
-        {
-            Form4 form4 = new Form4(8);
-            form4.Show();
-            this.Hide();
-        }
+            topPanel.Controls.AddRange(new Control[] { btnAddGroup, pictureBox1, label });
 
-        private void button9_Click(object sender, EventArgs e)
-        {
-            Form4 form4 = new Form4(9);
-            form4.Show();
-            this.Hide();
-        }
+            // FlowLayoutPanel для групп (вместо TableLayoutPanel - проще)
+            FlowLayoutPanel flowPanel = new FlowLayoutPanel();
+            flowPanel.Dock = DockStyle.Fill;
+            flowPanel.FlowDirection = FlowDirection.TopDown;
+            flowPanel.WrapContents = false;
+            flowPanel.AutoScroll = true;
+            flowPanel.Padding = new Padding(10);
 
-        private void button10_Click(object sender, EventArgs e)
-        {
-            Form4 form4 = new Form4(10);
-            form4.Show();
-            this.Hide();
-        }
+            // Сохраняем ссылку на flowPanel вместо mainTable
+            mainTable = new TableLayoutPanel(); // Если хотите использовать TableLayoutPanel
+            mainTable.Dock = DockStyle.Fill;
+            mainTable.AutoScroll = true;
+            mainTable.AutoSize = true;
+            mainTable.ColumnCount = 1;
+            mainTable.RowCount = 0;
+            mainTable.Padding = new Padding(10);
 
-        private void button11_Click(object sender, EventArgs e)
-        {
-            Form4 form4 = new Form4(11);
-            form4.Show();
-            this.Hide();
-        }
+            // Панель с прокруткой
+            Panel scrollPanel = new Panel();
+            scrollPanel.Dock = DockStyle.Fill;
+            scrollPanel.AutoScroll = true;
+            scrollPanel.Controls.Add(mainTable);
 
-        private void button12_Click(object sender, EventArgs e)
-        {
-            Form4 form4 = new Form4(12);
-            form4.Show();
-            this.Hide();
+            // Добавляем на форму
+            this.Controls.Add(scrollPanel);
+            this.Controls.Add(topPanel);
         }
-
-        private void button13_Click(object sender, EventArgs e)
+        
+        private void Item_PropertyChanged(object sender, PropertyChangedEventArgs e)
         {
-            Form4 form4 = new Form4(13);
-            form4.Show();
-            this.Hide();
-        }
-
-        private void button14_Click(object sender, EventArgs e)
-        {
-            Form4 form4 = new Form4(14);
-            form4.Show();
-            this.Hide();
-        }
-
-        private void button15_Click(object sender, EventArgs e)
-        {
-            Form4 form4 = new Form4(15);
-            form4.Show();
-            this.Hide();
-        }
-
-        private void button16_Click(object sender, EventArgs e)
-        {
-            Form4 form4 = new Form4(16);
-            form4.Show();
-            this.Hide();
-        }
-
-        private void button17_Click(object sender, EventArgs e)
-        {
-            Form4 form4 = new Form4(17);
-            form4.Show();
-            this.Hide();
-        }
-
-        private void button18_Click(object sender, EventArgs e)
-        {
-            Form4 form4 = new Form4(18);
-            form4.Show();
-            this.Hide();
-        }
-
-        private void button19_Click(object sender, EventArgs e)
-        {
-            Form4 form4 = new Form4(19);
-            form4.Show();
-            this.Hide();
-        }
-
-        private void button20_Click(object sender, EventArgs e)
-        {
-            Form4 form4 = new Form4(20);
-            form4.Show();
             this.Hide();
         }
     }
---
+++ DnD_character_list/GroupOfCharecters.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using static DnD_character_list.Form3;
+
+namespace DnD_character_list
+{
+    internal class GroupOfCharecters : IDisposable
+    {
+        public Button Button { get; private set; }
+        public Label Label { get; private set; }
+        public Button DeleteButton { get; private set; }
+        public Panel Panel { get; private set; }
+        public GroupBox GroupBox { get; private set; }
+        public int Index { get; set; }
+        private bool _isClosed { get; set; } = false;
+        public bool IsClosed
+        {
+            get => _isClosed;
+            set
+            {
+                if (_isClosed)
+                {
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event Action<GroupOfCharecters> OnDelete;
+
+        public GroupOfCharecters()
+        {
+            
+
+            // Основная кнопка персонажа
+            Button = new Button();
+            Button.Size = new Size(180, 50);
+            Button.Location = new Point(10, 12);
+            Button.Text = $"Персонаж {Index}";
+            Button.Click += Button_Click;
+
+            // Панель для группы
+            Panel = new Panel();
+            Panel.Size = new Size(200, 70);
+            Panel.Margin = new Padding(0, 0, 0, 10);
+
+            GroupBox = new GroupBox();
+            GroupBox.Size = new Size(200, 70);
+            GroupBox.Location = new Point(0, 0);
+
+            // Label с информацией
+            Label = new Label();
+            Label.Location = new Point(190, 10);
+            Label.Size = new Size(200, 25);
+            Label.Text = "Информация о персонаже";
+            Label.TextAlign = ContentAlignment.MiddleLeft;
+
+            // Кнопка удаления
+            DeleteButton = new Button();
+            DeleteButton.Text = "🗑️";
+            DeleteButton.Size = new Size(10, 10);
+            DeleteButton.Location = new Point(190, 10);
+            DeleteButton.Click += DeleteButton_Click;
+            DeleteButton.BackColor = Color.LightCoral;
+
+            GroupBox.Controls.AddRange(new Control[] { Button, Label, DeleteButton });
+            Panel.Controls.Add(GroupBox);
+
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Form4 form4 = new Form4(Index);
+            form4.Show();
+            IsClosed = true;
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            OnDelete?.Invoke(this);
+        }
+
+        public void Dispose()
+        {
+            Button?.Dispose();
+            Label?.Dispose();
+            DeleteButton?.Dispose();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
